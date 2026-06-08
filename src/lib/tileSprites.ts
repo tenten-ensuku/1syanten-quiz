2f05aab74e6d5a08559aa60ea1a21bd70311c16e
showtamu <82339016+wataruM2001@users.noreply.github.com>
2026-06-08 19:25:07 +0900

    Use suit tile sprites

--- src/lib/tileSprites.ts
+++ src/lib/tileSprites.ts
@@ -1,7 +1,12 @@
 import { TileId } from "./quizData";
 
 export type SpriteInfo = {
-  file: "tiles-sprite.png" | "tiles-yoko-sprite.png";
+  file:
+    | "tiles-man.png"
+    | "tiles-pin.png"
+    | "tiles-sou.png"
+    | "tiles-honor.png"
+    | "tiles-yoko-sprite.png";
   sheetWidth: number;
   sheetHeight: number;
   cellWidth: number;
@@ -10,38 +15,7 @@ export type SpriteInfo = {
   y: number;
 };
 
-const TILE_ORDER: TileId[] = [
-  "1m",
-  "2m",
-  "3m",
-  "4m",
-  "5m",
-  "6m",
-  "7m",
-  "8m",
-  "9m",
-  "1p",
-  "2p",
-  "3p",
-  "4p",
-  "5p",
-  "6p",
-  "7p",
-  "8p",
-  "9p",
-  "1s",
-  "2s",
-  "3s",
-  "4s",
-  "5s",
-  "6s",
-  "7s",
-  "8s",
-  "9s",
-  "hatsu"
-];
-
-const NORMAL_COLUMNS = 7;
+const NORMAL_COLUMNS = 9;
 const NORMAL_CELL_WIDTH = 8;
 const NORMAL_CELL_HEIGHT = 12;
 
@@ -70,12 +44,31 @@ export function getSpriteInfo(tileId: TileId, sideways: boolean): SpriteInfo | n
     };
   }
 
-  const index = TILE_ORDER.indexOf(tileId);
+  if (tileId === "hatsu") {
+    return {
+      file: "tiles-honor.png",
+      sheetWidth: NORMAL_CELL_WIDTH,
+      sheetHeight: NORMAL_CELL_HEIGHT,
+      cellWidth: NORMAL_CELL_WIDTH,
+      cellHeight: NORMAL_CELL_HEIGHT,
+      x: 0,
+      y: 0
+    };
+  }
+
+  const suit = tileId[1];
+  const index = Number(tileId[0]) - 1;
+  const file =
+    suit === "m"
+      ? "tiles-man.png"
+      : suit === "p"
+        ? "tiles-pin.png"
+        : "tiles-sou.png";
 
   return {
-    file: "tiles-sprite.png",
+    file,
     sheetWidth: NORMAL_COLUMNS * NORMAL_CELL_WIDTH,
-    sheetHeight: Math.ceil(TILE_ORDER.length / NORMAL_COLUMNS) * NORMAL_CELL_HEIGHT,
+    sheetHeight: NORMAL_CELL_HEIGHT,
     cellWidth: NORMAL_CELL_WIDTH,
     cellHeight: NORMAL_CELL_HEIGHT,
     x: (index % NORMAL_COLUMNS) * NORMAL_CELL_WIDTH,
